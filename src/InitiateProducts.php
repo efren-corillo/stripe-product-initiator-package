d30b308a51ed1c1a7d706686f79884e81f898404
efren-corillo <efren.corillo@doublybear.dev>
2023-05-09 23:00:12 +0800

    initial commit, composer init, and project core files

---
+++ src/InitiateProducts.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Doublybear\StripeProductInitiatorPackage;
+
+use AllowDynamicProperties;
+use Stripe;
+use Stripe\Exception\ApiErrorException;
+
+#[AllowDynamicProperties] class InitiateProducts
+{
+    protected Stripe\StripeClient $stripe;
+
+    /**
+     * this will check if what plan to create
+     * @param string $key Stripe Secret Key
+     * @param array $products
+     * @return array An array of product keys.
+     * @throws ApiErrorException
+     */
+    public function createProduct(string $key, array $products)
+    {
+        $this->stripe = new Stripe\StripeClient($key);
+
+        $productIds = [];
+
+        foreach ($products as $key => $product) {
+            // Create a Product
+            $productIds[$key] = $prodRes = $this->stripe->products->create($product['product']);
+
+            // using the product id create a price for the product.
+            foreach ($product['prices'] as $price) {
+                // add product id.
+                array_unshift($price, ["product" => $prodRes->id]);
+                // check if the price is set to default.
+                if (in_array("default_price", $price)) {
+                    // pop the default_price
+                    array_pop($price);
+
+                    // create price
+                    $priceRes = $this->stripe->prices->create($price);
+                    // using price_id update the product for the default price.
+                    $this->stripe->products->update(
+                        $prodRes->id,
+                        ['default_price' => $priceRes->id]
+                    );
+
+                } else {
+                    // just create price.
+                    $priceRes = $this->stripe->prices->create($price);
+                }
+
+                $productIds[$key]["price"] = $priceRes;
+            }
+        }
+
+        return $productIds;
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'doublybear/stripe-product-initiator-package',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'doublybear/stripe-product-initiator-package' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.12.1',
+            'version' => '10.12.1.0',
+            'reference' => '9e8055d55e8212ad844f48d7cf525b630689be35',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
